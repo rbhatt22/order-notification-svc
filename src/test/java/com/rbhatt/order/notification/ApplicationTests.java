a3cefff6521a8b087dc7defe521fdef8cfe00d70
rbhatt22 <rutvijbhatt22@gmail.com>
2026-01-07 20:19:58 -0500

    Intial push with spring boot and other dependecies

---
+++ src/test/java/com/rbhatt/order/notification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbhatt.order.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
